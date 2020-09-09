74f190d36a0adbf1807d21b42915d01ae11aa159
Ellison Leão <ellisonleao@gmail.com>
2020-09-09 13:12:37 -0300

    using nvim-web-devicons to get icon outputs

--- lua/el/extensions.lua
+++ lua/el/extensions.lua
@@ -1,6 +1,5 @@
 local Job = require('plenary.job')
 
-local log = require('el._log')
 local modes = require('el.data').modes
 local mode_highlights = require('el.data').mode_highlights
 local sections = require('el.sections')
@@ -69,176 +68,15 @@ extensions.mode = function(_, _)
   local higroup = mode_highlights[mode]
   local display_name = modes[mode][1]
 
-  return sections.highlight(
-    higroup,
-    string.format('[%s]', display_name)
-  )
+  return sections.highlight(higroup, string.format('[%s]', display_name))
 end
 
--- adding it as a function just for better clarity
--- on whats going on
-local file_extension = function(filename)
-  return filename:match('%.(%w+)$')
-end
-
-extensions.file_icon = function(_, bufnr)
-  local icons = {
-    default_symbol = '',
-    ai = '',
-    apache = '',
-    awk = '',
-    bash = '',
-    bat = '',
-    bazel = '',
-    bib = '',
-    c = '',
-    cc = '',
-    clisp = '',
-    clj = '',
-    cljc = '',
-    clojure = '',
-    cmake = '',
-    cobol = '',
-    coffee = ' ',
-    config = '',
-    coq = '',
-    cp = '',
-    cpp = '',
-    crystal = '',
-    csh = '',
-    csharp = '',
-    css = '',
-    cuda = '',
-    cxx = '',
-    cython = '',
-    d = '',
-    dart = '',
-    db = '',
-    diff = '',
-    dockerfile = '',
-    dump = '',
-    edn = '',
-    ejs = '',
-    elisp = '',
-    elixir = '',
-    elm = '',
-    erl = '',
-    fish = '',
-    fs = '',
-    fsi = '',
-    fsscript = '',
-    fsx = '',
-    go = '',
-    graphviz = '',
-    h = '',
-    hbs = '',
-    hh = '',
-    hpp = '',
-    hrl = '',
-    hs = '',
-    htm = '',
-    html = '',
-    hxx = '',
-    ico = '',
-    idris = '',
-    ini = '',
-    j = '',
-    jasmine = '',
-    java = '',
-    jl = '',
-    javascript = '',
-    json = '',
-    jsx = '',
-    julia = '⛬',
-    jupyter = '',
-    kotlin = '',
-    ksh = '',
-    labview = '',
-    less = '',
-    lhs = '',
-    lisp = 'λ',
-    llvm = '',
-    lsp = 'λ',
-    lua = '',
-    m = '',
-    markdown = '',
-    mathematica = '',
-    matlab = '',
-    max = '',
-    md = '',
-    meson = '',
-    ml = '',
-    mli = '',
-    mustache = '',
-    nginx = '',
-    nim = '',
-    nix = '',
-    nvcc = '',
-    nvidia = '',
-    octave = '',
-    opencl = '',
-    org = '',
-    patch = '',
-    perl6 = '',
-    php = '',
-    pl = '',
-    postgresql = '',
-    pp = '',
-    prolog = '',
-    ps = '',
-    ps1 = '',
-    psb = '',
-    psd = '',
-    py = '',
-    pyc = '',
-    pyd = '',
-    pyo = '',
-    python = '',
-    rb = '',
-    react = '',
-    reason = '',
-    rkt = '',
-    rlib = '',
-    rmd = '',
-    rs = '',
-    rss = '',
-    ruby = '',
-    rust = '',
-    sass = '',
-    scala = '',
-    scheme = 'λ',
-    scm = 'λ',
-    scrbl = '',
-    scss = '',
-    sh = '',
-    slim = '',
-    sln = '',
-    sql = '',
-    styl = '',
-    suo = '',
-    svg = '',
-    swift = '',
-    t = '',
-    tex = '',
-    toml = '',
-    ts = '',
-    tsx = '',
-    twig = '',
-    txt = 'e',
-    typescript = '',
-    vim = '',
-    vue = '﵂',
-    xul = '',
-    yaml = '',
-    yml = '',
-    zsh = '',
-  }
-  local ficon = icons[bufnr.filetype]
-  if ficon == nil then
-    local extension = file_extension(bufnr.name)
-    return icons[extension] or icons.default_symbol
-  end
-  return ficon
+extensions.file_icon = function(_, buffer)
+  local ok, icon = pcall(function()
+    return require('nvim-web-devicons').get_icon(buffer.name, buffer.filetype,
+                                                 {default = true})
+  end)
+  return ok and icon or ''
 end
 
 return extensions
